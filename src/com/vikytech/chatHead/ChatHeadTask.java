d82645241288bfc2c29b3b92732dd5cf5a7f7de9
vikytech <famous.vikhyath@gmail.com>
2014-04-13 14:06:50 +0530

    Initial Commit

---
+++ src/com/vikytech/chatHead/ChatHeadTask.java
@@ -0,0 +1,15 @@
+package com.vikytech.chatHead;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChatHeadTask extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
