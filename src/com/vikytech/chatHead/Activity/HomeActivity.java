4e8d0889436dd601b2bb9a8a473e6262c80a9066
vikytech <famous.vikhyath@gmail.com>
2014-04-13 17:48:09 +0530

    Gave a toggle button to turn on/off chat head

--- src/com/vikytech/chatHead/Activity/HomeActivity.java
+++ src/com/vikytech/chatHead/Activity/HomeActivity.java
@@ -3,15 +3,28 @@ package com.vikytech.chatHead.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
+import com.vikytech.chatHead.R;
 import com.vikytech.chatHead.Services.ChatHeadService;
 
 
 public class HomeActivity extends Activity {
 
+    private ToggleButton toggleChatHeadButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startService(new Intent(this, ChatHeadService.class));
+        setContentView(R.layout.home);
+        toggleChatHeadButton = (ToggleButton) findViewById(R.id.toggleChatHead);
+    }
+
+    public void toggleChatHead(View view) {
+        if (toggleChatHeadButton.getText().equals("Turn Off Chat Head"))
+            startService(new Intent(this, ChatHeadService.class));
+        else
+            stopService(new Intent(this, ChatHeadService.class));
     }
 }
 
