295e94c73cf28cd7074151487721a42d88833ba4
vikytech <famous.vikhyath@gmail.com>
2014-04-14 11:59:14 +0530

    Minor code Refactoring

--- src/com/vikytech/chatHead/Services/ChatHeadService.java
+++ src/com/vikytech/chatHead/Services/ChatHeadService.java
@@ -1,20 +1,23 @@
 package com.vikytech.chatHead.Services;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
-import android.view.Gravity;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.WindowManager;
+import android.view.*;
 import android.widget.ImageView;
 import com.vikytech.chatHead.R;
+
+import static android.view.MotionEvent.*;
+import static android.view.WindowManager.LayoutParams;
 
 public class ChatHeadService extends Service {
 
     private WindowManager windowManager;
     private ImageView chatHead;
+    private LayoutParams params;
+    private Context context;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -25,15 +28,16 @@ public class ChatHeadService extends Service {
     public void onCreate() {
         super.onCreate();
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        context = this;
 
-        chatHead = new ImageView(this);
+        chatHead = new ImageView(context);
         chatHead.setImageResource(R.drawable.vikytech_logo);
 
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+        params = new LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                LayoutParams.TYPE_PHONE,
+                LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT);
 
         params.gravity = Gravity.TOP | Gravity.LEFT;
@@ -51,15 +55,15 @@ public class ChatHeadService extends Service {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
+                    case ACTION_DOWN:
                         initialX = params.x;
                         initialY = params.y;
                         initialTouchX = motionEvent.getRawX();
                         initialTouchY = motionEvent.getRawY();
                         return true;
-                    case MotionEvent.ACTION_UP:
+                    case ACTION_UP:
                         return true;
-                    case MotionEvent.ACTION_MOVE:
+                    case ACTION_MOVE:
                         params.x = initialX + (int) (motionEvent.getRawX() - initialTouchX);
                         params.y = initialY + (int) (motionEvent.getRawY() - initialTouchY);
                         windowManager.updateViewLayout(chatHead, params);
